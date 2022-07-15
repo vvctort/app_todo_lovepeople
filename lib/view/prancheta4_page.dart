7653c6de02f2c0a46c2843cb465db77180e4f9e9
vvctort <victor123toma@gmail.com>
2022-07-15 17:31:53 -0300

    criei tela 4 e rotas

--- lib/view/prancheta4_page.dart
+++ lib/view/prancheta4_page.dart
@@ -8,7 +8,6 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +19,8 @@ class _ListPageState extends State<ListPage> {
                 Stack(
                   children: [
                     Container(
-                      width: 85,
-                      height: 85,
+                      width: 75,
+                      height: 75,
                       decoration: const BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.only(
@@ -39,9 +38,16 @@ class _ListPageState extends State<ListPage> {
                 const SizedBox(
                   width: 35,
                 ),
-                const Text(
-                  'Suas listagens',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
+                Column(
+                  children: const [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Suas listagens',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -65,23 +71,59 @@ class _ListPageState extends State<ListPage> {
                   border: InputBorder.none,
                   suffixIcon: IconButton(
                     onPressed: () {},
-                    icon: const Icon(Icons.search, size: 40),
+                    icon: const Icon(
+                      Icons.search,
+                      size: 35,
+                      color: Color.fromARGB(255, 49, 1, 185),
+                    ),
                   ),
                 ),
               ),
             ),
-            const Card(
-              child: ListTile(
-                title: Text(
-                  'Limpar a casa',
-                ),
-                style: ListTileStyle.drawer,
-              ),
+            const SizedBox(
+              height: 20,
             ),
+            taskCard(const Color.fromARGB(255, 255, 242, 204)),
+            const SizedBox(
+              height: 20,
+            ),
+            taskCard(const Color.fromARGB(255, 199, 255, 203)),
+            const SizedBox(
+              height: 20,
+            ),
+            taskCard(const Color.fromARGB(255, 232, 197, 255)),
           ],
         ),
       ),
       backgroundColor: const Color.fromARGB(255, 169, 1, 247),
+    );
+  }
+
+  Widget taskCard(text) {
+    return SizedBox(
+      height: 120,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        elevation: 0,
+        color: text,
+        child: Center(
+          child: ListTile(
+            title: const Text(
+              'Limpar a casa',
+            ),
+            subtitle: const Text(
+              'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore ‘erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+              maxLines: 4,
+              style: TextStyle(fontSize: 14),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
