8a1894d12fab54e947ab800d1290872fe9fb89c1
Mac <mac@Macs-MacBook-Pro.local>
2022-07-30 11:12:47 -0300

    tela login pronta

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:app_todo_lovepeople/view/prancheta4_page.dart';
+
+import 'package:app_todo_lovepeople/view/tela_login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'lista de tarefas',
-      home: ListPage(),
+      home: LoginPage(),
     );
   }
 }
--- lib/view/tela_login.dart
+++ lib/view/tela_login.dart
@@ -1,6 +1,9 @@
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -8,83 +11,164 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox
-         width: MediaQuery.of(context).size.width
-         height: MediaQuery.of(context).size.height
-         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-            TextField(
-             onChanged: (text){
-              email = text;
-            },
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-            labelText: 'email',
-            border: OutlineInputBorder(),
-          ),
+      backgroundColor: const Color.fromARGB(255, 169, 1, 247),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 2, bottom: 4),
+                  width: 500,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(200),
+                      bottomLeft: Radius.circular(200),
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: SizedBox(
+                          height: 180,
+                          width: 180,
+                          child: Image.asset(
+                            'assets/images/logo_coruja.png',
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 145),
+                        child: Text('Lovepeople', style: TextStyle(color: Color.fromARGB(255, 49, 1, 185),fontFamily: 'Montserrat-Bold'),),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30.0),
+                  child: Text('Que bom que voltou!', style: TextStyle(color: Colors.white,fontFamily: 'Montserrat-SemiBold',fontSize: 27),),
+                  
+                ),
+                
+              const  SizedBox(
+                  height: 10,
+                ),
+               Form(
+                key: _formKey,
+                child: Column(children: [
+                 Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                   child: TextFormField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    controller: emailController,
+                    validator: (value){
+                      if (value!.trim().isEmpty){
+                        return 'campo em branco';
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        labelText: 'Numero de telefone, email ou CPF',
+                        labelStyle: TextStyle(color: Color.fromARGB(255, 49, 1, 185)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                    ),
+                ),
+                 ),
+                const SizedBox(height: 20,),
+                 Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                   child: TextFormField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: 'Senha', 
+                        labelStyle: TextStyle(color: Color.fromARGB(255, 49, 1, 185)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                        suffixIcon: Icon(Icons.remove_red_eye)),
+                ),
+                 ),],),),
+                const SizedBox(
+                  height: 1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30,bottom: 10),
+                  child: Row(
+                    children: [
+                      const Text('Esqueceu seu login ou senha?',style: TextStyle(color:Colors.white),),
+                      TextButton(onPressed: (){}, child: const Text('Clique aqui',style: TextStyle(color: Color.fromARGB(255,255,214,0)),),
+
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                    //  if (email == 'mari_moreira1234@hotmail.com' &&
+                     //     password == '123') {
+                     //   
+                    //  } else {
+                      //  print('login invalido');
+                   //
+                      //}
+                      if (_formKey.currentState!.validate()){
+
+                      }
+                    },
+                    style: ButtonStyle (backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 49, 1, 185,),),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+                    side: const BorderSide(color: Colors.white)),),),
+                    child: const Text('Entrar',style: TextStyle(fontFamily: 'Montserrat-SemiBold'),),
+
+                  ),
+                ),
+               const SizedBox(height: 60),
+                Row(
+                 children: List.generate(150~/1, (index) => Expanded(
+                  child: Container(
+                   color: index%2==0?Colors.transparent
+                     :Colors.grey,
+                      height: 2,
+ ),
+ )),
+  ),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Nao possui cadastro?',style: TextStyle(color:Colors.white),),
+                    TextButton(onPressed: (){}, child: const Text('Clique aqui',style: TextStyle(color: Color.fromARGB(255,255,214,0)),),
+
+                    ),
+                  ],
+                ),
+              ],
+
+            ),
         ),
-       SizedBox(height: 10,)
-       TextField (
-        onChanged: (text) {
-          password = text;
-        },
-        obscureText: true,
-          decoration: const InputDecoration(
-            labelText: 'password',
-            border: OutlineInputBorder (),
-          ),
-       ),
-      const SizedBox( height: 15,),
-       ElevatedButton(
-        onPressed: () {
-          if (email == 'mari_moreira1234@hotmail.com' && password == '123') {
-          print ('correto');
-          } else {
-            print ('login invalido');
-               }
-              },
-              child:const Text ('Entrar'),
-            )
-           ],
-          ),
-         ),
+      ),
       ),
     );
   }
 }
-      
-       
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
