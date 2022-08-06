5280c013455323f345593f41f6a9dd255d8a97f8
WallaceGMoura <wallaceferrari30@gmail.com>
2022-08-06 14:49:33 -0300

    fiz conexão com api

--- lib/view/prancheta2_page.dart
+++ lib/view/prancheta2_page.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
+import 'package:app_todo_lovepeople/presenter/cadastro_presenter.dart';
+import 'package:app_todo_lovepeople/view/tela_login.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Cadastro extends StatefulWidget {
   const Cadastro({Key? key}) : super(key: key);
@@ -22,231 +27,246 @@ class _CadastroState extends State<Cadastro> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 169, 1, 247),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 100),
-            child: Column(children: [
-              const Text(
-                ' Vamos começar!',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Montserrat-SemiBold',
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Nome',
-                    labelStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    hintText: "Nome",
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    contentPadding: EdgeInsets.all(20),
-                  ),
-                  controller: _nomeController,
-                  keyboardType: TextInputType.name,
-                  validator: (nome) {
-                    if (nome == null || nome.isEmpty) {
-                      return 'Por favor, digite seu nome completo';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Número de telefone,e-mail ou CPF',
-                    labelStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    hintText: " Número de telefone,e-mail ou CPF",
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    contentPadding: EdgeInsets.all(20),
-                  ),
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (email) {
-                    if (email == null || email.isEmpty) {
-                      return 'Por favor, digite seu e-mail';
-                    } else if (!RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(_emailController.text)) {
-                      return 'Por favor, escreva um e-mail correto';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Senha',
-                    labelStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    hintText: " Senha ",
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    contentPadding: EdgeInsets.all(20),
-                  ),
-                  controller: _passworldController,
-                  keyboardType: TextInputType.text,
-                  validator: (senha) {
-                    if (senha == null || senha.isEmpty) {
-                      return 'Por favor, digite sua senha';
-                    } else if (senha.length < 6) {
-                      return 'Por favor, digite uma senha maior que 6 caracteres';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Confirme a Senha',
-                    labelStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    hintText: " Confirme a Senha ",
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(255, 49, 1, 185),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    contentPadding: EdgeInsets.all(20),
-                  ),
-                  controller: _cofirmeController,
-                  keyboardType: TextInputType.text,
-                  validator: (senha) {
-                    if (senha == null || senha.isEmpty) {
-                      return 'Por favor, confirme sua senha';
-                    } else if (senha.length < 6) {
-                      return 'Por favor, digite uma senha maior que 6 caracteres';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  FocusScopeNode currentFocus = FocusScope.of(context);
-                  if (_formKey.currentState!.validate()) {
-                    if (!currentFocus.hasPrimaryFocus) {
-                      currentFocus.unfocus();
-                    }
-                  }
-                  if (_formKey.currentState!.validate()) {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  }
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      const Color.fromARGB(255, 49, 1, 185)),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      side: const BorderSide(color: Colors.white),
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Cadastrar',
+      body: Consumer<CadastroPresenter>(builder: (_, presenter, snapshot) {
+        return Form(
+          key: _formKey,
+          child: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(top: 100),
+              child: Column(children: [
+                const Text(
+                  ' Vamos começar!',
                   style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                     fontFamily: 'Montserrat-SemiBold',
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 150,
-              ),
-              Row(
-                children: List.generate(
-                  150 ~/ 1,
-                  (index) => Expanded(
-                    child: Container(
-                      color: index % 2 == 0 ? Colors.transparent : Colors.grey,
-                      height: 2,
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      hintText: "Nome",
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    controller: _nomeController,
+                    keyboardType: TextInputType.name,
+                    validator: (nome) {
+                      if (nome == null || nome.isEmpty) {
+                        return 'Por favor, digite seu nome completo';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Número de telefone,e-mail ou CPF',
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      hintText: " Número de telefone,e-mail ou CPF",
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (email) {
+                      if (email == null || email.isEmpty) {
+                        return 'Por favor, digite seu e-mail';
+                      } else if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(_emailController.text)) {
+                        return 'Por favor, escreva um e-mail correto';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      hintText: " Senha ",
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    controller: _passworldController,
+                    keyboardType: TextInputType.text,
+                    validator: (senha) {
+                      if (senha == null || senha.isEmpty) {
+                        return 'Por favor, digite sua senha';
+                      } else if (senha.length < 6) {
+                        return 'Por favor, digite uma senha maior que 6 caracteres';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Confirme a Senha',
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      hintText: " Confirme a Senha ",
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 49, 1, 185),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    controller: _cofirmeController,
+                    keyboardType: TextInputType.text,
+                    validator: (senha) {
+                      if (senha == null || senha.isEmpty) {
+                        return 'Por favor, confirme sua senha';
+                      } else if (senha.length < 6) {
+                        return 'Por favor, digite uma senha maior que 6 caracteres';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScopeNode currentFocus = FocusScope.of(context);
+                    if (_formKey.currentState!.validate()) {
+                      if (!currentFocus.hasPrimaryFocus) {
+                        currentFocus.unfocus();
+                      }
+                    }
+                    if (_formKey.currentState!.validate()) {
+                      presenter.obterLogin(_emailController.text,
+                          _passworldController.text, _nomeController.text, () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage(),
+                            ));
+                      }, () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content:
+                                    Text('Erro no processo! Tente de novo')));
+                      });
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 49, 1, 185)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side: const BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cadastrar',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat-SemiBold',
                     ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Já possui cadastro?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                const SizedBox(
+                  height: 150,
+                ),
+                Row(
+                  children: List.generate(
+                    150 ~/ 1,
+                    (index) => Expanded(
+                      child: Container(
+                        color:
+                            index % 2 == 0 ? Colors.transparent : Colors.grey,
+                        height: 2,
+                      ),
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/login');
-                    },
-                    child: const Text(
-                      'Entrar',
-                      style: TextStyle(color: Colors.orange, fontSize: 16),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Já possui cadastro?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ]),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/login');
+                      },
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(color: Colors.orange, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
