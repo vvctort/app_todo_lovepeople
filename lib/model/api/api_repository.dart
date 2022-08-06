56f200d52ddab1625796fdb5ea9ee3418386eef0
WallaceGMoura <wallaceferrari30@gmail.com>
2022-08-06 14:29:57 -0300

    criei repository e presenter

--- lib/model/api/api_repository.dart
+++ lib/model/api/api_repository.dart
@@ -1,1 +1,24 @@
-class ApiTodo{}
+import 'dart:convert';
+
+import 'package:app_todo_lovepeople/model/api/json_repository.dart';
+import 'package:http/http.dart' as http;
+
+class ApiTodo {
+  String token = '';
+
+  Future<CadastroUsuariojson?> cadastro(
+      String email, String nome, String senha) async {
+    var url =
+        Uri.parse('https://todo-lovepeople.herokuapp.com/auth/local/register');
+    var response = await http.post(url, body: {
+      'username': nome,
+      'e-mail': email,
+      'senha': senha,
+    });
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      LoginDeUsuario resp = LoginDeUsuario.fromJson(json);
+      var token = resp.jwt!;
+    }
+  }
+}
